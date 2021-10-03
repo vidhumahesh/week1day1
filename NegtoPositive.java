77f3775a139d9c9ab435f33f36753686c2673db1
vidhumahesh <34765991+vidhumahesh@users.noreply.github.com>
2021-10-03 13:44:14 +0530

    Add files via upload

---
+++ NegtoPositive.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class NegtoPositive {
+
+	public static void main(String[] args) {
+		int number = -40;
+		
+		if(number<0) {
+			number = number * (-1);
+			
+		}
+		System.out.println("Number : " + number);
+
+	}
+
+}
